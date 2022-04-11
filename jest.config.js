8c8eb24eee349c4cd782a7fd4403ac18023f142e
aleveha <21022001qQ>
2022-04-11 09:03:03 +0200

    feat: add utils tests #0

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import("ts-jest/dist/types").InitialOptionsTsJest} */
+// eslint-disable-next-line no-undef
+module.exports = {
+	rootDir: "./",
+	preset: "ts-jest",
+	testEnvironment: "node",
+};
